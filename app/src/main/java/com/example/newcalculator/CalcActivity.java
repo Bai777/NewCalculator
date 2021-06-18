73778a8574258a6246220a11e82552a0eac238e8
Stepan <bai_777@mail.ru>
2021-06-18 15:26:40 +0300

    New Calculator

---
+++ app/src/main/java/com/example/newcalculator/CalcActivity.java
@@ -0,0 +1,16 @@
+package com.example.newcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+
+public class CalcActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calc);
+    }
+}
---
+++ app/src/main/java/com/example/newcalculator/ScrolView.java
@@ -0,0 +1,58 @@
+package com.example.newcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.EditText;
+
+
+
+
+public class ScrolView extends AppCompatActivity {
+
+    private EditText editTextTwo;
+    private EditText editTextThree;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrol_view);
+
+        editTextTwo = findViewById(R.id.edit2);
+        editTextThree = findViewById(R.id.edit3);
+
+        editTextTwo.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (v.getId() == R.id.edit2) {
+                    v.getParent().requestDisallowInterceptTouchEvent(true);
+                    switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                        case MotionEvent.ACTION_UP:
+                            v.getParent().requestDisallowInterceptTouchEvent(false);
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
+
+        editTextThree.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (v.getId() == R.id.edit3) {
+                    v.getParent().requestDisallowInterceptTouchEvent(true);
+                    switch (event.getAction() & MotionEvent.ACTION_MASK) {
+                        case MotionEvent.ACTION_UP:
+                            v.getParent().requestDisallowInterceptTouchEvent(false);
+                            break;
+                    }
+                }
+                return false;
+            }
+        });
+    }
+
+
+}
