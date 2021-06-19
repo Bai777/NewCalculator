03c9df4f0e023dbabd14d59c54aad5a090cd89f9
Stepan <bai_777@mail.ru>
2021-06-19 11:43:06 +0300

    Добавил обработку кнопки на главном экране. "Начнём вычисления". Button = id/btnMianLayout https://developer.android.com/reference/android/widget/Button

--- app/src/main/java/com/example/newcalculator/MainActivity.java
+++ app/src/main/java/com/example/newcalculator/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.newcalculator;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button btnMainDisplay;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnMainDisplay = findViewById(R.id.btnMianLayout);
+        btnMainDisplay.setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnMianLayout:
+                Intent intent = new Intent(this, CalcActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                Toast.makeText(this, "Something wrong", Toast.LENGTH_SHORT).show();
+        }
     }
 }
