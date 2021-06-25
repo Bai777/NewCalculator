bc07ad3849df821e475be592c6230f3c645a12e3
Stepan <bai_777@mail.ru>
2021-06-25 18:10:03 +0300

    Создал SettingActivityChangeThemeApp для смены тем активити.

---
+++ app/src/main/java/com/example/newcalculator/SettingActivityChangeThemeApp.java
@@ -0,0 +1,14 @@
+package com.example.newcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingActivityChangeThemeApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_change_theme_app);
+    }
+}
