39111fae09efede75a57fcaa70c0d6e039efc492
Stepan <bai_777@mail.ru>
2021-06-25 17:12:46 +0300

    Создал новую активити SettingActivityThemeApp с настройками, где есть возможность выбрать тему приложения.

---
+++ app/src/main/java/com/example/newcalculator/SettingActivityThemeApp.java
@@ -0,0 +1,14 @@
+package com.example.newcalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingActivityThemeApp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting_theme_app);
+    }
+}
