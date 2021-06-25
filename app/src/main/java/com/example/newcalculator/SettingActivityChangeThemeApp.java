a7ffbadda9fbac4a37c680e7075c84ce1cf1985e
Stepan <bai_777@mail.ru>
2021-06-25 19:13:44 +0300

    Создал поля класса. Метод инициализации кнопок.

--- app/src/main/java/com/example/newcalculator/SettingActivityChangeThemeApp.java
+++ app/src/main/java/com/example/newcalculator/SettingActivityChangeThemeApp.java
@@ -3,12 +3,31 @@ package com.example.newcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class SettingActivityChangeThemeApp extends AppCompatActivity {
+
+    private static final String SHARED_PREF = "KEY";
+
+    private static final String appTheme = "APP_THEME";
+    private static final int MY_STYLE = 0;
+    private static final int LIGHT_STYLE = 1;
+    private static final int DARK_STYLE = 2;
+    private static final int CUSTOM_STYLE = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_setting_change_theme_app);
+
+        initThemeChooser();
     }
+
+    private void initThemeChooser()
+    {
+
+    }
+
+
 }
